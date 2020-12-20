656b8a6b956f7989d330430bee37a3751b21b8ab
denis <yudaev1@gmail.com>
2020-12-20 13:49:54 +0300

    Add "welcome_user"

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,11 +2,12 @@
 
 """main script"""
 
-from brain_games import cli #import of function 'welcome_user' 
+from brain_games import cli
 
-def main(): 
-    print('Welcome to the Brain Games!') 
-    cli.welcome_user() 
+
+def main():
+    print('Welcome to the Brain Games!')
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
