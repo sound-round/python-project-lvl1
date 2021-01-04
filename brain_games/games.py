ae79f268c00b04ed2fd45baf96b77ccf7c8a8a8e
denis <yudaev1@gmail.com>
2021-01-04 23:31:28 +0300

    Update games.py

--- brain_games/games.py
+++ brain_games/games.py
@@ -3,31 +3,26 @@ import random
 
 
 def greeting():
-    """Function greets player, asks his name and returns it"""
+    """Function greets player, asks his name and returns it."""
     print('Welcome to The Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
     return name
 
 
-def run_brain_even():
-    """Function runs brain-even games"""
-
-    def get_correct_answer(number):
-        """Function defines correct answer for random number"""
-        if number % 2 == 0:
-            return 'yes'
-        return 'no'
+def run_engine(function, randomiser, string):
+    """Function runs a game.
+    The first argument is a function, defining a correct answer.
+    The second argument is a function, returning a question.
+    The third argument shows a task, which player has to solve."""
 
     name = greeting()
-
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-
+    print(string)
     counter = 0
     while counter < 3:
-        number = random.randint(1, 100)
-        correct_answer = get_correct_answer(number)
-        print('Question: {}'.format(number))
+        question = randomiser()
+        correct_answer = function(question)
+        print('Question: {}'.format(question))
         user_answer = prompt.string('Your answer: ')
 
         if user_answer == correct_answer:
@@ -39,3 +34,40 @@ def run_brain_even():
                                                   correct_answer, name))
             counter = 0
     print('Congratulations, {}!'.format(name))
+
+
+def run_brain_even():
+    """Function defines conditions of the brain-even game."""
+
+    def get_correct_answer(number):
+        """Function defines correct answer for a random number."""
+
+        if number % 2 == 0:
+            return 'yes'
+        return 'no'
+
+    def randomiser():
+        """Function returns a random number from 1 to 100."""
+        return random.randint(1, 100)
+
+    string = 'Answer "yes" if the number is even, otherwise answer "no".'
+    run_engine(get_correct_answer, randomiser, string)
+
+
+def run_brain_calc():
+    """Function defines conditions of the brain-calc game."""
+
+    def get_correct_answer(question):
+        """Function defines correct answer for an expression"""
+
+        return str(eval(question))
+
+    def randomiser():
+        """Function returns an expression"""
+        number_1 = random.randint(1, 100)
+        number_2 = random.randint(1, 100)
+        sign = random.choice(['+', '-', '*'])
+        return '{} {} {}'.format(number_1, sign, number_2)
+
+    string = 'What is the result of the expression?'
+    run_engine(get_correct_answer, randomiser, string)
