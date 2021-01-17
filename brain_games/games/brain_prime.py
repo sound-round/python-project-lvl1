4c7ba4fb6d379124904e9360ac67af3a36633cd5
denis <yudaev1@gmail.com>
2021-01-17 19:26:16 +0300

    Rebuild brain-prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,31 +1,20 @@
 import random
-from brain_games import engine
 
 
-GAME_TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def run():
     """Function defines conditions of the brain-prime game."""
 
-    def randomize_number():
-        """Function returns a random number from 1 to 100."""
+    question = random.randint(1, 100)
+    correct_answer = 'yes'
 
-        return random.randint(1, 100)
-
-    def is_prime(random_number):
-        """Function defines if the random number is prime."""
-
-        divisors = list(range(2, 101))
-        # excludes the random number from the divisors
-        if random_number != 1:
-            divisors.pop(divisors.index(random_number))
-        for divisor in divisors:
-            if random_number % divisor == 0:
-                correct_answer = 'no'
-                return correct_answer
-
-        correct_answer = 'yes'
-        return correct_answer
-
-    engine.run(is_prime, randomize_number, GAME_TASK)
+    divisors = list(range(2, 101))
+    # excludes the random number from the divisors
+    if question != 1:
+        divisors.pop(divisors.index(question))
+    for divisor in divisors:
+        if question % divisor == 0:
+            correct_answer = 'no'
+    return question, correct_answer
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,11 @@
 #!usr/bin/env python
 
+from brain_games import engine
 from brain_games.games import brain_prime
 
 
 def main():
-    brain_prime.run()
+    engine.run(brain_prime)
 
 
 if __name__ == '__main__':
