ebbed78df83e17ef9856175303e4a6ed9a4cf2d2
denis <yudaev1@gmail.com>
2021-01-17 19:05:48 +0300

    Rebuild brain-calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,25 +1,19 @@
 import random
-from brain_games import engine
+import operator
 
 
-GAME_TASK = 'What is the result of the expression?'
+TASK = 'What is the result of the expression?'
 
 
 def run():
     """Function defines conditions of the brain-calc game."""
 
-    def randomize_expression():
-        """Function returns an expression for solving it."""
-
-        random_number_1 = random.randint(1, 100)
-        random_number_2 = random.randint(1, 100)
-        random_operator = random.choice(['+', '-', '*'])
-        return '{} {} {}'.format(random_number_1,
-                                 random_operator, random_number_2)
-
-    def get_correct_answer(generated_question):
-        """Function defines correct answer for an expression"""
-
-        return str(eval(generated_question))
-
-    engine.run(get_correct_answer, randomize_expression, GAME_TASK)
+    random_number_1 = random.randint(1, 100)
+    random_number_2 = random.randint(1, 100)
+    random_operator, random_sign = random.choice([(operator.add, '+'),
+                                                  (operator.sub, '-'),
+                                                  (operator.mul, '*')])
+    question = '{} {} {}'.format(random_number_1,
+                                 random_sign, random_number_2)
+    correct_answer = str(random_operator(random_number_1, random_number_2))
+    return question, correct_answer
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,11 @@
 #!usr/bin/env python
 
+from brain_games import engine
 from brain_games.games import brain_calc
 
 
 def main():
-    brain_calc.run()
+    engine.run(brain_calc)
 
 
 if __name__ == '__main__':
