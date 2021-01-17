c6dfa590055ab10024420d7e7319c51164b1f342
denis <yudaev1@gmail.com>
2021-01-17 18:48:46 +0300

    Rebuild engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,20 +1,16 @@
 import prompt
 
 
-def run(get_correct_answer, randomize_question, GAME_TASK):
-    """Function runs a game.
-    The first argument is a function, defining a correct answer.
-    The second argument is a function, returning a question.
-    The third argument shows a task, which player has to solve."""
+def run(game):
+    """Function runs a game."""
 
     print('Welcome to The Brain Games!')
     player_name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(player_name))
-    print(GAME_TASK)
+    print(game.GAME_TASK)
     for win in range(0, 3):
-        generated_question = randomize_question()
-        correct_answer = get_correct_answer(generated_question)
-        print('Question: {}'.format(generated_question))
+        question, correct_answer = game.run()
+        print('Question: {}'.format(question))
         user_answer = prompt.string('Your answer: ')
 
         if user_answer == correct_answer:
