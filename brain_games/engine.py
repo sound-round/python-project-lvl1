c66153021cf2a298767d3461d07baaefc55fee86
denis <yudaev1@gmail.com>
2021-01-17 15:18:44 +0300

    delete "else" in cycle "for"

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,7 +1,7 @@
 import prompt
 
 
-def run(get_correct_answer, question_randomizer, GAME_TASK):
+def run(get_correct_answer, randomize_question, GAME_TASK):
     """Function runs a game.
     The first argument is a function, defining a correct answer.
     The second argument is a function, returning a question.
@@ -14,7 +14,7 @@ def run(get_correct_answer, question_randomizer, GAME_TASK):
     wins_counter = 0
     WINS_LIMIT = 3
     while wins_counter < WINS_LIMIT:
-        generated_question = question_randomizer()
+        generated_question = randomize_question()
         correct_answer = get_correct_answer(generated_question)
         print('Question: {}'.format(generated_question))
         user_answer = prompt.string('Your answer: ')
@@ -22,9 +22,11 @@ def run(get_correct_answer, question_randomizer, GAME_TASK):
         if user_answer == correct_answer:
             print('Correct!')
             wins_counter += 1
-        else:
-            print("'{}' is wrong answer ;(. Correct answer was '{}'."
-                  "\nLet's try again, {}!".format(user_answer,
-                                                  correct_answer, player_name))
-            wins_counter = 0
+            continue
+
+        print("'{}' is wrong answer ;(. Correct answer was '{}'."
+              "\nLet's try again, {}!".format(user_answer,
+                                              correct_answer, player_name))
+        wins_counter = 0
+
     print('Congratulations, {}!'.format(player_name))
