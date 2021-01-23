bf524d9018b22e64df3d25d8ebf52d8e01c50aaa
denis <yudaev1@gmail.com>
2021-01-23 14:00:42 +0300

    Simplify the structure of engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,19 +8,15 @@ def run(game):
     player_name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(player_name))
     print(game.TASK)
-    for win in range(0, 3):
+    for _ in range(0, 3):
         question, correct_answer = game.run()
         print('Question: {}'.format(question))
         user_answer = prompt.string('Your answer: ')
 
-        if user_answer == correct_answer:
-            print('Correct!')
-            continue
-
-        print("'{}' is wrong answer ;(. Correct answer was '{}'."
-              "\nLet's try again, {}!".format(user_answer,
-                                              correct_answer, player_name))
-        break
-
-    if user_answer == correct_answer:
-        print('Congratulations, {}!'.format(player_name))
+        if user_answer != correct_answer:
+            print("'{}' is wrong answer ;(. Correct answer was '{}'."
+                  "\nLet's try again, {}!".format(user_answer,
+                                                  correct_answer, player_name))
+            return
+        print('Correct!')
+    print('Congratulations, {}!'.format(player_name))
