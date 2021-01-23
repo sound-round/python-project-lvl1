8017eec61dbbcedaba05a728a6da15bbbaae55dc
denis <yudaev1@gmail.com>
2021-01-23 19:27:58 +0300

    Update games/brain_gcd.py

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,15 +1,29 @@
 import random
-import math
 
 
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
-def run():
-    """Function defines conditions of the brain-gcd game."""
-
+def get_question():
     random_number_1 = random.randint(1, 100)
     random_number_2 = random.randint(1, 100)
     question = '{} {}'.format(random_number_1, random_number_2)
-    correct_answer = str(math.gcd(random_number_1, random_number_2))
+
+    return question, random_number_1, random_number_2
+
+
+def get_correct_answer(random_number_1, random_number_2):
+    while random_number_1 > 0 and random_number_2 > 0:
+        if random_number_1 > random_number_2:
+            random_number_1 %= random_number_2
+        else:
+            random_number_2 %= random_number_1
+    return str(random_number_1 + random_number_2)
+
+
+def run():
+    """Function defines conditions of the brain-gcd game."""
+
+    question, random_number_1, random_number_2 = get_question()
+    correct_answer = get_correct_answer(random_number_1, random_number_2)
     return question, correct_answer
