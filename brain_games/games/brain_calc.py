0d08fe11d407f7b729d8c3caddec25f839e3a9a2
denis <yudaev1@gmail.com>
2021-01-23 14:57:27 +0300

    Update brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -8,12 +8,18 @@ DESCRIPTION = 'What is the result of the expression?'
 def run():
     """Function defines conditions of the brain-calc game."""
 
+    def get_correct_answer(random_number_1, random_number_2, random_operator):
+        correct_answer = str(random_operator(random_number_1, random_number_2))
+        return correct_answer
+
     random_number_1 = random.randint(1, 100)
     random_number_2 = random.randint(1, 100)
-    random_operator, random_sign = random.choice([(operator.add, '+'),
-                                                  (operator.sub, '-'),
-                                                  (operator.mul, '*')])
+    operators = [(operator.add, '+'), (operator.sub, '-'), (operator.mul, '*')]
+    random_operator, random_sign = random.choice(operators)
     question = '{} {} {}'.format(random_number_1,
                                  random_sign, random_number_2)
-    correct_answer = str(random_operator(random_number_1, random_number_2))
+
+    correct_answer = get_correct_answer(random_number_1,
+                                        random_number_2,
+                                        random_operator)
     return question, correct_answer
