dcb3ea462aa5430f30a505d990e82bcdc548e9b1
denis <yudaev1@gmail.com>
2021-01-23 18:59:02 +0300

    Update games/brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,17 +4,26 @@ import random
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def get_question():
+    return random.randint(1, 100)
+
+
+def get_correct_answer(predicate):
+    return 'yes' if predicate else 'no'
+
+
+def is_prime(question):
+    divisor = 2
+    while question % divisor != 0:
+        divisor += 1
+    return divisor == question
+
+
 def run():
     """Function defines conditions of the brain-prime game."""
 
-    question = random.randint(1, 100)
-    correct_answer = 'yes'
+    question = get_question()
+    predicate = is_prime(question)
+    correct_answer = get_correct_answer(predicate)
 
-    divisors = list(range(2, 101))
-    # excludes the random number from the divisors
-    if question != 1:
-        divisors.pop(divisors.index(question))
-    for divisor in divisors:
-        if question % divisor == 0:
-            correct_answer = 'no'
     return question, correct_answer
