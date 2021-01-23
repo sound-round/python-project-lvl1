13cf063a825ecd5a14002f58d5b5a4f94bf3c586
denis <yudaev1@gmail.com>
2021-01-23 18:58:22 +0300

    Update games/brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -5,21 +5,27 @@ import operator
 DESCRIPTION = 'What is the result of the expression?'
 
 
-def run():
-    """Function defines conditions of the brain-calc game."""
-
-    def get_correct_answer(random_number_1, random_number_2, random_operator):
-        correct_answer = str(random_operator(random_number_1, random_number_2))
-        return correct_answer
-
+def get_question():
     random_number_1 = random.randint(1, 100)
     random_number_2 = random.randint(1, 100)
     operators = [(operator.add, '+'), (operator.sub, '-'), (operator.mul, '*')]
     random_operator, random_sign = random.choice(operators)
     question = '{} {} {}'.format(random_number_1,
                                  random_sign, random_number_2)
+    return [question, random_number_1, random_number_2,
+            random_operator]
 
+
+def get_correct_answer(random_number_1, random_number_2, random_operator):
+    correct_answer = str(random_operator(random_number_1, random_number_2))
+    return correct_answer
+
+
+def run():
+    """Function defines conditions of the brain-calc game."""
+
+    [question, random_number_1, random_number_2,
+     random_operator] = get_question()
     correct_answer = get_correct_answer(random_number_1,
-                                        random_number_2,
-                                        random_operator)
+                                        random_number_2, random_operator)
     return question, correct_answer
