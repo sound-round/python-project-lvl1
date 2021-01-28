6b24914a202a125ac5d06b6d46786b1ec2ee0cb3
denis <yudaev1@gmail.com>
2021-01-28 23:06:04 +0300

    Update games/brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,19 +1,13 @@
 import random
 import operator
 
-
 DESCRIPTION = 'What is the result of the expression?'
 
 
-def get_question():
-    random_number_1 = random.randint(1, 100)
-    random_number_2 = random.randint(1, 100)
-    operators = [(operator.add, '+'), (operator.sub, '-'), (operator.mul, '*')]
-    random_operator, random_sign = random.choice(operators)
+def get_question(random_number_1, random_number_2, random_sign):
     question = '{} {} {}'.format(random_number_1,
                                  random_sign, random_number_2)
-    return [question, random_number_1, random_number_2,
-            random_operator]
+    return question
 
 
 def get_correct_answer(random_number_1, random_number_2, random_operator):
@@ -24,8 +18,12 @@ def get_correct_answer(random_number_1, random_number_2, random_operator):
 def round():
     """Function defines conditions of the brain-calc game."""
 
-    [question, random_number_1, random_number_2,
-     random_operator] = get_question()
+    random_number_1 = random.randint(1, 100)
+    random_number_2 = random.randint(1, 100)
+    operators = [(operator.add, '+'), (operator.sub, '-'), (operator.mul, '*')]
+    random_operator, random_sign = random.choice(operators)
+
+    question = get_question(random_number_1, random_number_2, random_sign)
     correct_answer = get_correct_answer(random_number_1,
                                         random_number_2, random_operator)
     return question, correct_answer
