02827cf0fb3577e6051abed28c97612a80c44bcf
denis <yudaev1@gmail.com>
2021-01-31 15:05:34 +0300

    Update games/brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -4,15 +4,14 @@ import random
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even(question):
-    return question % 2 == 0
+def is_even(number):
+    return number % 2 == 0
 
 
-def round():
+def start_round():
     """Function defines conditions of the brain-even game."""
 
     question = random.randint(1, 100)
-    predicate = is_even(question)
-    correct_answer = 'yes' if predicate else 'no'
+    correct_answer = 'yes' if is_even(question) else 'no'
 
     return question, correct_answer
