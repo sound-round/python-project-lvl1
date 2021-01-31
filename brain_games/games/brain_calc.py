692fddc59f85c6d80063a85e7695d6820e4b3636
denis <yudaev1@gmail.com>
2021-01-31 19:33:52 +0300

    Update games/brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -4,17 +4,27 @@ import operator
 DESCRIPTION = 'What is the result of the expression?'
 MIN_NUMBER = 1
 MAX_NUMBER = 100
+OPERATORS = ['+', '-', '*']
+MAP_OPERATOR_TO_OPERATION = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul
+}
+
+
+def calculate_answer(first_number, second_number, operator):
+    return MAP_OPERATOR_TO_OPERATION[operator](first_number, second_number)
 
 
 def start_round():
     """Function defines conditions of the brain-calc game."""
 
-    random_number_1 = random.randint(MIN_NUMBER, MAX_NUMBER)
-    random_number_2 = random.randint(MIN_NUMBER, MAX_NUMBER)
-    operators = [(operator.add, '+'), (operator.sub, '-'), (operator.mul, '*')]
-    random_operator, random_sign = random.choice(operators)
-
-    question = '{} {} {}'.format(random_number_1,
-                                 random_sign, random_number_2)
-    correct_answer = str(random_operator(random_number_1, random_number_2))
+    first_random_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+    second_random_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+    random_operator = random.choice(OPERATORS)
+    question = '{} {} {}'.format(first_random_number,
+                                 random_operator, second_random_number)
+    correct_answer = str(calculate_answer(first_random_number,
+                                          second_random_number,
+                                          random_operator))
     return question, correct_answer
