8317469a635c4c9e6dca6c67ddeefe52b158409f
denis <yudaev1@gmail.com>
2021-01-31 15:06:40 +0300

    Update games/brain_progression.py

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,42 +1,41 @@
 import random
 import copy
 
+
 DESCRIPTION = 'What number is missing in the progression?'
 GAP = '..'
+MIN_LENGTH = 5
+MAX_LENGTH = 10
+MAX_STEP = 5
+MIN_STEP = 1
 
 
-def get_progression(first_number, step, length):
-    progression = [str(first_number + step * i) for i in range(length)]
+def get_progression(start, step, length):
+    progression = [str(start + step * i) for i in range(length)]
     return progression
 
 
-def get_correct_answer(progression, index_hidden_element):
+def get_question(progression, hidden_element_index):
     copy_progression = copy.copy(progression)
-    correct_answer = copy_progression.pop(index_hidden_element)
-    return correct_answer
-
-
-def get_question(progression, index_hidden_element):
-    copy_progression = copy.copy(progression)
-    copy_progression[index_hidden_element] = GAP
+    copy_progression[hidden_element_index] = GAP
     question = ' '.join(copy_progression)
     return question
 
 
-def round():
+def start_round():
     """Function defines conditions of the brain-progression game."""
 
     # defines the first number of a progression.
-    first_number = random.randint(1, 20)
+    start = random.randint(1, 20)
     # defines a step of a progression.
-    step = random.randint(1, 5)
+    step = random.randint(MIN_STEP, MAX_STEP)
     # defines length of a progression.
-    length = random.randint(5, 10)
-    progression = get_progression(first_number, step, length)
+    length = random.randint(MIN_LENGTH, MAX_LENGTH)
+    progression = get_progression(start, step, length)
 
-    index_hidden_element = random.randint(0, len(progression) - 1)
+    hidden_element_index = random.randint(0, len(progression) - 1)
 
-    question = get_question(progression, index_hidden_element)
-    correct_answer = get_correct_answer(progression, index_hidden_element)
+    question = get_question(progression, hidden_element_index)
+    correct_answer = progression[hidden_element_index]
 
     return question, correct_answer
